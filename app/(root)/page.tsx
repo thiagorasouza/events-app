79d86351e28e179e4a0e1c9e05b226c32db18bbb
Thiago Souza <thiagorasouza@gmail.com>
2024-01-07 02:01:01 +0000

    feat: implement responsive footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-3 py-2 bg-slate-100 min-h-screen">
+    <div className="container mx-auto flex flex-col px-3 py-2 bg-slate-200 min-h-screen">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ components/shared/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="mt-auto flex flex-col text-center md:flex-row md:justify-between gap-2 bg-white">
+      <p>&#169; Events App {new Date().getFullYear()}.</p>
+      <p>This project is under development by Thiago Souza.</p>
+    </footer>
+  );
+}
+
+export default Footer;
