88bbc83d31a5551e39eec5b1c43a841ae9a4a2a9
Thiago Souza <thiagorasouza@gmail.com>
2024-01-18 08:22:32 -0500

    feat: create events page

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 function NavItems() {
   return (
     <ul className="flex flex-col gap-2 md:flex-row md:gap-3">
-      <li>Home</li>
-      <li>Create Event</li>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/events/create">Create Event</Link>
+      </li>
       <li>My Profile</li>
     </ul>
   );
