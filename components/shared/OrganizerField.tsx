31c37bcdb5a54310554a63aff919f8a6ec87072b
Thiago Souza <thiagorasouza@gmail.com>
2024-01-28 12:44:17 -0500

    feat: make OrganizerField display current user image and name

--- components/shared/OrganizerField.tsx
+++ components/shared/OrganizerField.tsx
@@ -1,18 +1,37 @@
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 function OrganizerField() {
+  const { user, isLoaded, isSignedIn } = useUser();
+
   return (
     <div>
       <div className="text-sm font-medium mb-1">Organizer:</div>
       <div className="flex gap-3 items-center bg-white p-2 rounded-md text-sm">
-        <Image
-          src="https://source.unsplash.com/random/128x128/?profile"
-          alt="profile image"
-          className="rounded-full"
-          width={24}
-          height={24}
-        />
-        <span>Thiago Souza</span>
+        {isLoaded ? (
+          isSignedIn ? (
+            <>
+              <Image
+                src={user.imageUrl}
+                alt="profile image"
+                className="rounded-full"
+                width={24}
+                height={24}
+              />
+              <span>{user.fullName}</span>
+            </>
+          ) : (
+            <>
+              <div className="rounded-full bg-gray-500 w-6 h-6"></div>
+              <span>Unable to retrieve current user</span>
+            </>
+          )
+        ) : (
+          <>
+            <div className="rounded-full bg-gray-500 w-6 h-6"></div>
+            <span>Loading...</span>
+          </>
+        )}
       </div>
     </div>
   );
