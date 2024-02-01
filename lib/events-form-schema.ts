93057c071dabdf2a78ba68c068fb37b9c1c69413
Thiago Souza <thiagorasouza@gmail.com>
2024-02-01 18:09:36 -0500

    feat: detail zod FormSchema

--- lib/events-form-schema.ts
+++ lib/events-form-schema.ts
@@ -1,13 +1,16 @@
 import * as z from "zod";
 
+const now = new Date();
+const oneYearAhead = new Date(new Date().getFullYear() + 1);
+
 export const formSchema = z.object({
-  title: z.string(),
-  location: z.string(),
-  startDateTime: z.date(),
-  endDateTime: z.date(),
-  description: z.string(),
-  external_url: z.string(),
-  image_url: z.string(),
+  title: z.string().min(3).max(100),
+  location: z.string().min(3).max(200),
+  startDateTime: z.date().min(now).max(oneYearAhead),
+  endDateTime: z.date().min(now).max(oneYearAhead),
+  description: z.string().max(10000),
+  external_url: z.string().url(),
+  image_url: z.string().url(),
   categoryId: z.string(),
 });
 
