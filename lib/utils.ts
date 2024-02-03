4726400fd988484c85c466adbcc93cf5f5dde43f
Thiago Souza <thiagorasouza@gmail.com>
2024-02-03 10:36:43 -0500

    feat: utility date functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,34 @@
 import { type ClassValue, clsx } from "clsx";
+import {
+  format,
+  differenceInMilliseconds,
+  differenceInHours,
+  differenceInDays,
+  differenceInWeeks,
+  differenceInMonths,
+  differenceInMinutes,
+  formatDistance,
+} from "date-fns";
 import { twMerge } from "tailwind-merge";
 
+type DateType = string | number | Date;
+
 export type KeyOfType<Source, Type> = keyof {
-  [Property in keyof Source as Source[Property] extends Type
-    ? Property
-    : never]: any;
+  [Property in keyof Source as Source[Property] extends Type ? Property : never]: any;
 };
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function formatDateTime(date: string | number | Date) {
+  if (new Date(date).getFullYear === new Date().getFullYear) {
+    return format(date, "EEEE, MMMM d, 'at' h:mm a");
+  } else {
+    return format(date, "MMMM d, yyyy 'at' h:mm a");
+  }
+}
+
+export function formatTimeDifference(startDate: DateType, endDate: DateType) {
+  return formatDistance(endDate, startDate);
 }
